3e967f417eaf0c6d9015738ed701af85cf221a29
Austin Stankowski <austinharris085@gmail.com>
2025-02-12 14:35:25 -0500

    Implemented Terminal Inputs to Run Functions

--- caesarCipher.py
+++ caesarCipher.py
@@ -4,7 +4,7 @@ def cipherChar(char, shiftNumber):
     for i in range(len(letterBase)):
         if char == letterBase[i]:
             newLetter = letterBase[(i + shiftNumber) % 26]
-    return newLetter
+            return newLetter
 
 def cipherString(string, shiftNumber):
     updatedWords = []
@@ -25,7 +25,7 @@ def cipherString(string, shiftNumber):
                 char = cipherChar(char, shiftNumber)
                 newChars.append(char)
 
-            #Handles Symbols & Numbers
+            #Don't Encrypt (Numbers, Symbols, etc)
             else:
                 newChars.append(char)
 
@@ -36,15 +36,66 @@ def cipherString(string, shiftNumber):
     
     return stringOut
 
-
-
 def bruteDecrypt(string):
     for i in range(26):
-        print(cipherString(string, i))
+        decryptAttempt = cipherString(string, i)
+        print(f"Shifted {26-i}: {decryptAttempt}")
 
 def decrypt(string, shiftNumber):
-    print(cipherString(string, 26-shiftNumber))
+    print(cipherString(string, 26-(shiftNumber % 26)))
 
 
+def displayMenu(options):
+    for key, value in options.items():
+        print(f"[{key}] {value}")
+
+def getUserChoice(prompt, validChoices):
+    while True:
+        choice = input(prompt).strip()
+        if choice in validChoices:
+            return choice
+        else:
+            print("Invalid, please try again.")
+
+def encryptMenu():
+    message = input("Enter the message you'd like to encrypt:")
+    shift = int(input("Enter the shift amount:"))
+    print("Encrypted Message: ", cipherString(message, shift))
+
+def decryptMenu():
+    decryptOptions = {"1": "Specific Decryption", "2": "Brute Force Decryption", "3": "Back"}
+
+    while True:
+        print("\n Decryption Options:")
+        displayMenu(decryptOptions)
+        
+        choice = getUserChoice("Choose an Option:", decryptOptions.keys())
+
+        if choice == "1":
+            message = input("Enter the message you'd like to decrypt: ")
+            shift = int(input("Enter the original shift amount: "))
+            print("Decrypted Message:", cipherString(message, 26-(shift%26)))
+        elif choice == "2":
+            message = input("Enter the message you'd like to decrypt: ")
+            bruteDecrypt(message)
+        elif choice == "3":
+            break
 
 
+def main():
+    options = {"1": "Encrypt", "2": "Decrypt", "3": "Quit"}
+    while True:
+        print("\nWelcome to the Caesar Cipher Encoder & Decoder!")
+        displayMenu(options)
+        choice = getUserChoice("Enter your choice: ", options.keys())
+
+        if choice == "1":
+            encryptMenu()
+        elif choice == "2":
+            decryptMenu()
+        elif choice == "3":
+            print("Goodbye!")
+            break
+
+if __name__ == "__main__":
+    main()

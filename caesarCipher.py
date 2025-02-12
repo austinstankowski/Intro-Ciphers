576b98a12a1579d41e9eaba34ff830907788532a
Austin Stankowski <austinharris085@gmail.com>
2025-02-11 20:18:32 -0500

    Implemented Function 'cipherChar'

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,7 +1,18 @@
-letterBase = ['A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S']
-def cipherChar(letter, shiftNumber):
+letterBase = ['A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 
+'''
+[cipherChar]
 
+Inputs:
+char = Capital Letter
+shiftNumber = Positive Integer
 
-    return output
+return: Capital Letter
+'''
+
+def cipherChar(char, shiftNumber):
+    for i in range(len(letterBase)):
+        if char == letterBase[i]:
+            newLetter = letterBase[(i + shiftNumber) % 25]
+    return newLetter
